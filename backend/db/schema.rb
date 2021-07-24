e02f3658ad92bfece4db1aabf8313c25974d919a
Luis Ernesto Gil Castellanos <luegilca@unal.edu.co>
2021-07-24 12:45:29 -0500

    Configuration for heroku deploy

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_19_035842) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "devices", force: :cascade do |t|
     t.integer "device_id"
     t.float "max_capacity_ml"
@@ -25,10 +28,11 @@ ActiveRecord::Schema.define(version: 2021_07_19_035842) do
   end
 
   create_table "usages", force: :cascade do |t|
-    t.integer "device_id"
+    t.bigint "device_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["device_id"], name: "index_usages_on_device_id"
   end
 
+  add_foreign_key "usages", "devices"
 end
